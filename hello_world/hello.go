054afc763f0e1dbaa161d97e18a12a105285544b
Drew Cook <drewcook29@gmail.com>
2021-07-27 23:39:16 -0600

    cleanup

---
+++ hello_world/hello.go
@@ -0,0 +1,10 @@
+// package declaration
+package main
+
+// import a package, 'fmt' ships with the Go standard library
+import "fmt"
+
+func main() {
+	// print hello world to the screen
+	fmt.Printf("hello, world\n")
+}
